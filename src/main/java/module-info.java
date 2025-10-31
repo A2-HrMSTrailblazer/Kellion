a58cc12f52af47553b130c4ec2435da1cc7ab5a5
HrMSTrailblazer <aungkhaingphyo_1@cmu.ac.th>
2025-10-31 16:48:50 +0700

    misc

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,6 @@
 module se233.kellion {
     requires javafx.controls;
     requires javafx.fxml;
-    requires javafx.graphics;
 
     opens se233.kellion to javafx.fxml;
     exports se233.kellion;
